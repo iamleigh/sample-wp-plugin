53cbcafc9bc19b0c339c61e26898b47228c5d9ee
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-08-01 10:55:24 -0500

    build

--- gulpfile.js
+++ gulpfile.js
@@ -4,27 +4,27 @@
  *
  * @since 1.0.0
  */
-const fs = require("fs");
-const gulp = require("gulp");
+const fs = require('fs');
+const gulp = require('gulp');
 
-const clean = require("gulp-clean");
-const copy = require("gulp-copy");
-const archiver = require("gulp-archiver");
+const clean = require('gulp-clean');
+const copy = require('gulp-copy');
+const archiver = require('gulp-archiver');
 
-const makepot = require("gulp-wp-pot");
-const gettext = require("gulp-gettext");
+const makepot = require('gulp-wp-pot');
+const gettext = require('gulp-gettext');
 
-const phpcs = require("gulp-phpcs"); // @todo: Include in a `test` task
-const phpunit = require("gulp-phpunit"); // @todo: Include in a `test` task
+// const phpcs = require('gulp-phpcs'); // @todo: Include in a `test` task
+// const phpunit = require('gulp-phpunit'); // @todo: Include in a `test` task
 
 /**
  * Package (pkg)
  *
- * @desc Call `package.json` file.
+ * @description Call `package.json` file.
  *
  * @since 1.0.0
  */
-const pkg = require("./package.json");
+const pkg = require('./package.json');
 
 /**
  * Development Paths & Files
@@ -32,26 +32,32 @@ const pkg = require("./package.json");
  * @since 1.0.0
  */
 const srcFiles = {
-	phpcs: ["leighton-quito.php", "leighton-quito-loader.php"],
-	makepot: ["leighton-quito.php", "leighton-quito-loader.php", "includes/**/*.php", "api/**/*.php"],
+	phpcs: ['leighton-quito.php', 'leighton-quito-loader.php'],
+	makepot: [
+		'leighton-quito.php',
+		'leighton-quito-loader.php',
+		'includes/**/*.php',
+		'api/**/*.php',
+	],
 };
 
 /**
  * Makepot
  *
- * @desc Gulp task to create POT files.
+ * @description Gulp task to create POT files.
  *
  * @since 1.0.0
  */
-gulp.task("makepot", () =>
+gulp.task('makepot', () =>
 	gulp
 		.src(srcFiles.makepot)
 		.pipe(
 			makepot({
 				domain: pkg.name,
-				package: "Leighton Quito",
-				bugReport: "https://github.com/iamleigh/leighton-quito-amp/issues",
-				metadataFile: "leighton-quito.php",
+				package: 'Leighton Quito',
+				bugReport:
+					'https://github.com/iamleigh/leighton-quito-amp/issues',
+				metadataFile: 'leighton-quito.php',
 			})
 		)
 		.pipe(gulp.dest(`languages/${pkg.name}.pot`))
@@ -60,62 +66,70 @@ gulp.task("makepot", () =>
 /**
  * Get Text
  *
- * @desc Grab languages files.
+ * @description Grab languages files.
  *
  * @since 1.0.0
  */
-gulp.task("gettext", () => gulp.src("languages/*.po").pipe(gettext()).pipe(gulp.dest("languages")));
+gulp.task('gettext', () =>
+	gulp.src('languages/*.po').pipe(gettext()).pipe(gulp.dest('languages'))
+);
 
 /**
  * Translate (i18n)
  *
- * @desc Run `makepot` and `gettext` tasks in parallel.
+ * @description Run `makepot` and `gettext` tasks in parallel.
  *
  * @since 1.0.0
  */
-gulp.task("i18n", gulp.parallel(["makepot", "gettext"]));
+gulp.task('i18n', gulp.parallel(['makepot', 'gettext']));
 
 /**
  * Package
  *
- * @desc Pack the plugin for release.
+ * @description Pack the plugin for release.
  *
  * @since 1.0.0
  */
 gulp.task(
-	"package",
+	'package',
 	gulp.series(
-		() => gulp.src(["dist", pkg.name, `${pkg.name}.*.zip`], { read: false, allowEmpty: true }).pipe(clean()),
+		() =>
+			gulp
+				.src(['dist', pkg.name, `${pkg.name}.*.zip`], {
+					read: false,
+					allowEmpty: true,
+				})
+				.pipe(clean()),
 
 		() =>
 			gulp
 				.src([
-					"leighton-quito.php",
-					"leighton-quito-loader.php",
-					"uninstall.php",
-					"LICENSE",
-					"README.md",
-					"api/**/*",
-					"assets/css/**/*",
-					"assets/images/**/*",
-					"assets/js/**/*",
-					"languages/**",
-					"includes/**/*",
-					"vendor/autoload.php",
-					"vendor/bin/**/*",
-					"vendor/composer/**/*",
-					"vendor/myclabs/**/*",
+					'leighton-quito.php',
+					'leighton-quito-loader.php',
+					'uninstall.php',
+					'LICENSE',
+					'README.md',
+					'api/**/*',
+					'assets/css/**/*',
+					'assets/images/**/*',
+					'assets/js/**/*',
+					'languages/**',
+					'includes/**/*',
+					'vendor/autoload.php',
+					'vendor/bin/**/*',
+					'vendor/composer/**/*',
+					'vendor/myclabs/**/*',
 				])
-				.pipe(copy(pkg.name)),
+				.pipe(gulp.dest(pkg.name)),
 
 		() =>
 			gulp
-				.src(pkg.name + "/**/*", { base: "." })
+				.src(pkg.name + '/**/*', { base: '.' })
 				.pipe(archiver(`${pkg.name}.${pkg.version}.zip`))
-				.pipe(gulp.dest(".")),
+				.pipe(gulp.dest('.')),
 
 		(done) =>
-			fs.rename(pkg.name, "dist", (err) => {
+			fs.rename(pkg.name, 'dist', (err) => {
 				if (err) throw err;
 				done();
 			})
@@ -125,28 +139,39 @@ gulp.task(
 /**
  * Challenge Package
  *
- * @desc Pack the plugin to submit for challenge.
+ * @description Pack the plugin to submit for challenge.
  *
  * @since 1.0.0
  */
 gulp.task(
-	"challenge",
+	'challenge',
 	gulp.series(
 		() =>
 			gulp
-				.src(["challenge", pkg.name, `${pkg.name}-challenge.*.zip`], { read: false, allowEmpty: true })
+				.src(['challenge', pkg.name, `${pkg.name}-challenge.*.zip`], {
+					read: false,
+					allowEmpty: true,
+				})
 				.pipe(clean()),
-
-		() => gulp.src(["**/*", "!dist/**/*", "!challenge/**/*", "!node_modules/**/*"]).pipe(copy(pkg.name)),
 
 		() =>
 			gulp
-				.src(pkg.name + "/**/*", { base: "." })
+				.src([
+					'**/*',
+					'!dist/**/*',
+					'!challenge/**/*',
+					'!node_modules/**/*',
+				])
+				.pipe(copy(pkg.name)),
+
+		() =>
+			gulp
+				.src(pkg.name + '/**/*', { base: '.' })
 				.pipe(archiver(`${pkg.name}-challenge.${pkg.version}.zip`))
-				.pipe(gulp.dest(".")),
+				.pipe(gulp.dest('.')),
 
 		(done) =>
-			fs.rename(pkg.name, "challenge", (err) => {
+			fs.rename(pkg.name, 'challenge', (err) => {
 				if (err) throw err;
 				done();
 			})
